254c9ce4766fdf3f8ffa3714d376c9670c48d298
Jesusmmz <jesusmmz17@hotmail.com>
2016-04-06 13:40:10 +0200

    Mostrado mensaje de "Activar GPS" si el GPS esta desactivado al pulsar el boton de MyLocation

--- app/src/main/java/com/jj/futbol/MapsActivity.java
+++ app/src/main/java/com/jj/futbol/MapsActivity.java
@@ -1,11 +1,14 @@
 package com.jj.futbol;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-
+import android.view.Gravity;
+import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,7 +16,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +35,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         //Activo el boton de Mi Ubicacion
         map.setMyLocationEnabled(true);
-        if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+        if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             map.setMyLocationEnabled(true);
+            map.setOnMyLocationButtonClickListener(this);
         }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+
+        LocationManager locManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        //Si el GPS no está habilitado
+        if (!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast toast = Toast.makeText(getBaseContext(),"Activa el GPS para ir a tu ubicación",Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            toast.show();
+        }
+        return false;
     }
 }
