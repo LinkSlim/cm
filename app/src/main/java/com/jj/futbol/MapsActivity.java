b5226aa1740ac272a32b345c52ec5bac817018b5
Jesusmmz <jesusmmz17@hotmail.com>
2016-04-14 21:08:29 +0200

    Implementado el widget PlacePicker de la API de Google Places

--- app/src/main/java/com/jj/futbol/MapsActivity.java
+++ app/src/main/java/com/jj/futbol/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.jj.futbol;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -8,12 +9,18 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,7 +28,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, View.OnClickListener{
+
+    private static final int REQUEST_PLACE_PICKER = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +39,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        /**
+         * Ejemplo de integracion con Firebase
+         * En este caso se esta realizando una lectura de un dato alojado en Firebase
+         **/
         Firebase.setAndroidContext(this);
         Firebase myFirebaseRef = new Firebase("https://appjj.firebaseio.com/");
         //myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
-
-        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() {
+        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() { //Agrego un listener al dato llamado "message"
             @Override
-            public void onDataChange(DataSnapshot snapshot) {
+            public void onDataChange(DataSnapshot snapshot) { //Cuando el valor del dato "message" cambia, se ejecuta este codigo
                 System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
                 Toast toast = Toast.makeText(getBaseContext(),snapshot.getValue().toString(),Toast.LENGTH_LONG);
                 toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
@@ -46,6 +60,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onCancelled(FirebaseError error) {
             }
         });
+
+        /*Listener para que el boton me lleve a la Activity de PlacePicker*/
+        final Button button = (Button) findViewById(R.id.AbrirPlacePicker);
+        button.setOnClickListener(this);
 
 
     }
@@ -78,6 +96,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return false;
     }
 
+    @Override
+    public void onClick(View v) {
+        // do something when the button is clicked
+        // Yes we will handle click here but which button clicked??? We don't know
+
+        // So we will make
+        switch (v.getId() /*to get clicked view id**/) {
+            case R.id.AbrirPlacePicker:
+
+                // BEGIN_INCLUDE(intent)
+            /* Use the PlacePicker Builder to construct an Intent.
+            Note: This sample demonstrates a basic use case.
+            The PlacePicker Builder supports additional properties such as search bounds.
+             */
+                try {
+                    PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+                    Intent intent = intentBuilder.build(this);
+                    // Start the Intent by requesting a result, identified by a request code.
+                    startActivityForResult(intent, REQUEST_PLACE_PICKER);
+
+                    // Hide the pick option in the UI to prevent users from starting the picker
+                    // multiple times.
+                    //showPickAction(false);
+
+                } catch (GooglePlayServicesRepairableException e) {
+                    GooglePlayServicesUtil.getErrorDialog(e.getConnectionStatusCode(), this, 0);
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    Toast.makeText(this, "Google Play Services is not available.",
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
+
+// END_INCLUDE(intent)
+
+                break;
+
+            default:
+                break;
+        }
+    }
 
 
 }
+
+
+
+
