34dc98c921d44d0bb8cbec4d5889e0ab817ae9c1
Jesusmmz <jesusmmz17@hotmail.com>
2016-04-15 19:59:23 +0200

    Recuperada informacion del lugar que se pica en el PlacePickery mostrada en un Toast

--- app/src/main/java/com/jj/futbol/MapsActivity.java
+++ app/src/main/java/com/jj/futbol/MapsActivity.java
@@ -20,6 +20,7 @@ import com.firebase.client.ValueEventListener;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,9 +29,9 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, View.OnClickListener{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, View.OnClickListener {
 
-    private static final int REQUEST_PLACE_PICKER = 1;
+    private static final int PLACE_PICKER_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +48,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Firebase.setAndroidContext(this);
         Firebase myFirebaseRef = new Firebase("https://appjj.firebaseio.com/");
         //myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
-        myFirebaseRef.child("message").addValueEventListener(new ValueEventListener() { //Agrego un listener al dato llamado "message"
+        myFirebaseRef.child("message").child("otro2").addValueEventListener(new ValueEventListener() { //Agrego un listener al dato llamado "message"
             @Override
             public void onDataChange(DataSnapshot snapshot) { //Cuando el valor del dato "message" cambia, se ejecuta este codigo
                 System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
-                Toast toast = Toast.makeText(getBaseContext(),snapshot.getValue().toString(),Toast.LENGTH_LONG);
+                Toast toast = Toast.makeText(getBaseContext(), snapshot.getValue().toString(), Toast.LENGTH_LONG);
                 toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
                 toast.show();
             }
@@ -61,7 +62,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-        /*Listener para que el boton me lleve a la Activity de PlacePicker*/
+        /*Agrego al boton un Listener para que el boton me lleve a la Activity de PlacePicker al ser pulsado (onClick)*/
         final Button button = (Button) findViewById(R.id.AbrirPlacePicker);
         button.setOnClickListener(this);
 
@@ -77,7 +78,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         //Activo el boton de Mi Ubicacion
         map.setMyLocationEnabled(true);
-        if ( ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             map.setMyLocationEnabled(true);
             map.setOnMyLocationButtonClickListener(this);
         }
@@ -86,10 +87,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public boolean onMyLocationButtonClick() {
 
-        LocationManager locManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        LocationManager locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         //Si el GPS no está habilitado
         if (!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Toast toast = Toast.makeText(getBaseContext(),"Activa el GPS para ir a tu ubicación",Toast.LENGTH_LONG);
+            Toast toast = Toast.makeText(getBaseContext(), "Activa el GPS para ir a tu ubicación", Toast.LENGTH_LONG);
             toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
             toast.show();
         }
@@ -98,10 +99,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onClick(View v) {
-        // do something when the button is clicked
-        // Yes we will handle click here but which button clicked??? We don't know
-
-        // So we will make
         switch (v.getId() /*to get clicked view id**/) {
             case R.id.AbrirPlacePicker:
 
@@ -114,7 +111,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
                     Intent intent = intentBuilder.build(this);
                     // Start the Intent by requesting a result, identified by a request code.
-                    startActivityForResult(intent, REQUEST_PLACE_PICKER);
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
 
                     // Hide the pick option in the UI to prevent users from starting the picker
                     // multiple times.
@@ -137,7 +134,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format("Place: %s, ID: %s, Direccion: %s", place.getName(), place.getId(), place.getAddress());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
 }
 
 
